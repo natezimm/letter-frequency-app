02d9ff5200f4b9ab444565bd7483dea8c6ded8fd
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2018-12-11 21:37:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var width = 800;
+var height = 400;
+var barPadding = 10;
+var svg = d3.select("svg")
+                .attr("width", width)
+                .attr("height", height);
+
 d3.select("#reset")
     .on("click", function() {
       d3.selectAll(".letter")
@@ -15,10 +22,12 @@ d3.select("form")
       d3.event.preventDefault();
       var input = d3.select("input");
       var text = input.property("value");
+      var data = getFrequencies(text);
+      var barWidth = width / data.length - barPadding;
 
-      var letters = d3.select("#letters")
+      var letters = svg
                       .selectAll(".letter")
-                      .data(getFrequencies(text), function(d) {
+                      .data(data, function(d) {
                         return d.character;
                       });
 
@@ -27,17 +36,36 @@ d3.select("form")
         .exit()
         .remove();
 
-      letters
+      var letterEnter = letters
         .enter()
-        .append("div")
+        .append("g")
           .classed("letter", true)
-          .classed("new", true)
-        .merge(letters)
-          .style("width", "20px")
-          .style("line-height", "20px")
-          .style("margin-right", "5px")
+          .classed("new", true);
+
+      letterEnter.append("rect");
+      letterEnter.append("text");
+
+      letterEnter.merge(letters)
+        .select("rect")
+          .style("width", barWidth)
           .style("height", function(d) {
-            return d.count * 20 + "px";
+            return d.count * 20;
+          })
+          .attr("x", function(d, i) {
+            return ( barWidth + barPadding) * i;
+          })
+          .attr("y", function(d) {
+            return height - d.count * 20;
+          });
+
+      letterEnter.merge(letters)
+        .select("text")
+          .attr("x", function(d, i) {
+            return ( barWidth + barPadding ) * i + barWidth / 2;
+          })
+          .attr("text-anchor", "middle")
+          .attr("y", function(d) {
+            return height - d.count * 20 - 10;
           })
           .text(function(d) {
             return d.character;
